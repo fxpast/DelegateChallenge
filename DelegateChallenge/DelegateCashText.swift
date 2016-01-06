1d2c1d3e0bf26c1ef176fee5d4ffa5760eda279e
MacbookPRV <roger@MacbookPRV.local>
2016-01-06 18:51:20 +0100

    Ajout de classes delegate

---
+++ DelegateChallenge/DelegateCashText.swift
@@ -0,0 +1,9 @@
+//
+//  DelegateCashText.swift
+//  DelegateChallenge
+//
+//  Created by MacbookPRV on 06/01/2016.
+//  Copyright © 2016 Pastouret Roger. All rights reserved.
+//
+
+import Foundation
---
+++ DelegateChallenge/DelegateZipCode.swift
@@ -0,0 +1,9 @@
+//
+//  DelegateZipCode.swift
+//  DelegateChallenge
+//
+//  Created by MacbookPRV on 06/01/2016.
+//  Copyright © 2016 Pastouret Roger. All rights reserved.
+//
+
+import Foundation
